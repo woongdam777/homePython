0f750e2563bb71f15f53bcd79238576fac08131f
woong <woongdam777@gmail.com>
2023-03-15 23:09:29 +0900

    python_base

---
+++ 01_base/variable.py
@@ -0,0 +1,16 @@
+a,b = 100,200
+a, b = b, a + b
+
+print(a,b)
+
+text = "Welcome to Python!"
+
+print((text + "\n")*10)
+
+name = "강기남"
+phone_number = "010-1534-4563"
+address = "서울시 강남구"
+
+print(name,phone_number,address)
+
+
